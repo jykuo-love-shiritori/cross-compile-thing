831a17cda412f4defee739d1debdc0d6f1309a4c
Dragonfly911117 <55215882+Dragonfly911117@users.noreply.github.com>
2023-03-12 09:47:46 +0800

    Create Pins.h

---
+++ Pins.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+//can be 3.3V or 1.8V
+#define PIN_7 396
+#define PIN_12 392
+#define PIN_13 397
+#define PIN_18 481
+#define PIN_19 429
+#define PIN_21 428
+#define PIN_23 427
+#define PIN_29 398
+#define PIN_32 297
+#define PIN_33 398
+#define PIN_35 395
+#define PIN_38 394
+#define PIN_40 393
+
+//3.3V
+#define PIN_11 466
+#define PIN_15 255
+#define PIN_22 254
+#define PIN_31 298
+#define PIN_37 388

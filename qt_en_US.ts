346a51f4c29fcd9f188442f7b1b920e19a2a7f08
noobdy <htommyf12@gmail.com>
2023-03-17 13:50:10 +0800

    init qt

---
+++ qt_en_US.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/qt_autogen/include/ui_mainwindow.h" line="68"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <location filename="build/qt_autogen/include/ui_mainwindow.h" line="69"/>
+        <source>LED0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <location filename="build/qt_autogen/include/ui_mainwindow.h" line="70"/>
+        <source>LED1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="53"/>
+        <location filename="build/qt_autogen/include/ui_mainwindow.h" line="71"/>
+        <source>LED2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <location filename="build/qt_autogen/include/ui_mainwindow.h" line="72"/>
+        <source>LED3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

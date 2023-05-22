4e74610526e02596eb4d4388720d13430349e645
Team Leader <>
2023-05-23 02:33:37 +0800

    Add gpio.h and docs

---
+++ module/gpio.h
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: WTFPL */
+/*
+ * GPIO Interface for Kernel Module
+ *
+ * Author: Team Leader <>
+ */
+
+#ifndef SHREK_GPIO_H
+#define SHREK_GPIO_H
+
+#include <stdbool.h>
+
+/*
+ * GPIO Sysfs Interface for Userspace:
+ * https://www.kernel.org/doc/html/latest/admin-guide/gpio/sysfs.html
+ *
+ * stackoverflow: https://stackoverflow.com/a/63812033
+ */
+
+/**
+ * gpio_export - export control of a GPIO to userspace
+ * @pin: GPIO Pin#
+ *
+ * Return: true if success. false if not, and errno is set to indicate the error
+ * for write().
+ */
+static bool gpio_export(unsigned int pin);
+
+/**
+ * gpio_unexport - reverses the effect of exporting to userspace.
+ * @pin: GPIO Pin#
+ *
+ * Return: true if success. false if not, and errno is set to indicate the error
+ * for write().
+ */
+static bool gpio_unexport(unsigned int pin);
+
+/**
+ * gpio_set_dir - set direction of GPIO
+ * @pin: GPIO Pin#
+ * @dirStatus: direction, reads as either "in" or "out".
+ *
+ * Return: true if success. false if not, and errno is set to indicate the error
+ * for write().
+ */
+static bool gpio_set_dir(unsigned int gpio, const char *dirStatus);
+
+/**
+ * gpio_set_value - set value of GPIO
+ * @pin: GPIO Pin#
+ * @value: reads as either 0 (low) or 1 (high).
+ *
+ * If the GPIO is configured as an output, this value may be written;
+ * any nonzero value is treated as high.
+ *
+ * Return: true if success. false if not, and errno is set to indicate the error
+ * for write().
+ */
+static bool gpio_set_value(unsigned int gpio, const char *value);
+
+/**
+ * gpio_read_value - read value of GPIO
+ * @pin: GPIO Pin#
+ *
+ * Return: 1 if high. 0 if low;
+ */
+static int gpio_read_value(unsigned int gpio);
+
+#endif /* SHREK_GPIO_H */
+

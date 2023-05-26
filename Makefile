2273f918a26320c47470a9f3408ac07a2091e3a8
Team Leader <>
2023-05-26 16:02:36 +0800

    idk

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: lab7_semaphore
+	./lab7_semaphore 1 2 3 4
--- lab7_semaphore.c
+++ lab7_semaphore.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
 
-sem_t semaphore;
+#define LED_COUNT 4
 
-void* child(){
-    sem_wait(&semaphore);
-    print("GPIO:%d status:%c",);
-    sleep(1);
+sem_t semaphore;
+int counter = 2;
+
+typedef struct {
+	int gpio;
+	int stat;
+} Swamp;
+
+void* child(void *_swamp){
+	Swamp *swamp = (Swamp *)_swamp;
+	int i = counter;
+	while(i--) {
+		int temp = sem_wait(&semaphore);
+		printf("GPIO:%d i = %d\n",swamp->gpio, i);
+		printf("GPIO:%d status:%d\n", swamp->gpio, swamp->stat);
+		//printf("GPIO:%d count: %d\n", ++count);
+		//printf("count: %d\n", temp);
+	}
+	free(swamp);
+    //printf("GPIO:\n");
     pthread_exit(NULL);
 }
 
+//  int pthread_create(pthread_t *restrict thread,
+//                         const pthread_attr_t *restrict attr,
+//                         void *(*start_routine)(void *),
+//                         void *restrict arg);
+
 int main(int argc,char* argv[]){
-    sem_init(&semaphore,0,0);
+    sem_init(&semaphore,0, LED_COUNT);
     pthread_t t;
-    pthread_create(&t,NULL,child,NULL);
-    print("status:%c%c%c%c",&argv[0],&argv[1],&argv[2],&argv[3]);
-    sem_post(&semaphore);
-    sem_post(&semaphore);
-    sem_post(&semaphore);
-    sem_post(&semaphore);
+	for (int i = 0; i < LED_COUNT; ++i) {
+		Swamp *swamp = malloc(sizeof(Swamp));
+		swamp->gpio = i;
+		swamp->stat = 0;
+		pthread_create(&t,NULL,child,swamp);
+	}
+
+    // printf("status:%c%c%c%c\n",&argv[0],&argv[1],&argv[2],&argv[3]);
+
+	sem_post(&semaphore);
+    sleep(1);
+	printf("sleep");
+	sem_post(&semaphore);
+//	for (int i = 0; i < LED_COUNT; ++i) {
+//		sem_post(&semaphore);
+//		sem_post(&semaphore);
+//	}
 
     pthread_join(t,NULL);
     return 0;
